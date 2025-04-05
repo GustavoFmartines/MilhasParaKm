a253af41752757427266582e018a4c5745f77ed9
GustavoFmartines <gustavo.martines2003@outlook.com>
2025-04-05 19:59:03 -0300

    Foi criado um coversor de milhas para quilômetros.

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿        int milhas;
+        double quilometros = 1.609;
+        double quilometros2;
+
+    Console.Clear();
+
+        Console.ForegroundColor = ConsoleColor.DarkBlue;
+        Console.BackgroundColor = ConsoleColor.Cyan;
+
+Console.WriteLine("|___Conversor de milhas para Quilômetros!___|");
+
+        Console.ResetColor();
+
+Console.WriteLine(@"
+Digite sua quantidade de milhas: ");
+milhas = Convert.ToInt32(Console.ReadLine()!);
+
+        quilometros2 = milhas * quilometros;
+
+Console.WriteLine(@$"
+Suas {milhas} milhas são iguais a {quilometros2}km ");
